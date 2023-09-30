deccab8e77ebcb6611889c8bae6d2ab271d6eb69
42path <bvalkov2008@gmail.com>
2023-09-30 23:14:12 +0300

    из парижа с любовью

--- app_maximum/admin.py
+++ app_maximum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Advertisement
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'create_at', 'auction']
+    list_filter = ['auction', 'create_at']
+    actions = ['make_auction_as_false']
+
+@admin.action(description='Delete auction')
+def make_auction_as_false(self, request, queryset):
+    queryset.update(auction=False)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
