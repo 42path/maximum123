ec24f7a7648108b4c98f1e8dc057db788c2c9bde
42path <bvalkov2008@gmail.com>
2023-10-01 14:38:58 +0300

    из парижа с любовью

--- app_maximum/views.py
+++ app_maximum/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Advertisement
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    advertisements = Advertisement.objects.all()
+    context = {'advertisement': advertisement}
+    return render(request, 'index.html', context)
 
 def top_sellers(request):
     return render(request, 'top_sellers.html')
