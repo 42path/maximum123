8dd4d1e51d0efa4cfddbde39c4599dfc0ff2d7d6
42path <bvalkov2008@gmail.com>
2023-10-01 14:14:08 +0300

    из парижа с любовью

--- app_maximum/admin.py
+++ app_maximum/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from .models import Advertisement
 # Register your models here.
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'create_at', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'create_date', 'auction']
     list_filter = ['auction', 'create_at']
-    actions = ['make_auction_as_false']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('General', {'fields': ('title', 'description'),}),
+        ('Finance', {'fields': ('price', 'auction'),
+        'classes': ['collapse']})
 
-@admin.action(description='Delete auction')
-def make_auction_as_false(self, request, queryset):
-    queryset.update(auction=False)
+    )
 
+    @admin.action(description='Delete auction')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    @admin.action(description='Add auction')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
 
 admin.site.register(Advertisement, AdvertisementAdmin)
--- app_maximum/models.py
+++ app_maximum/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import *
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -8,3 +10,12 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Auction", help_text="Choose if auction right")
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    @admin.display(description='Date_build')
+    def create_date(self):
+        from django.utils import timezone
+        if self.create_date.date() == timezone.now().date:
+            create_time = self.create_at.time().strftime("%H:%M:%S")
+            return format_html(
+            '<span style="color: green; font-weight: bold;">Today in {}</span>', create_time
+        )
+        return self.create_at.strftime("%d.%m.%Y" in "%H:%M:%S")
